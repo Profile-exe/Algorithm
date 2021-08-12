166ca3a19cb6c3231cbc3f8beb13867c5407124f
Profile-exe <kang67572346@gmail.com>
2021-08-12 16:12:18 +0900

    Move Edge class to Dijkstra class Make Edge class to Inner static class

--- graph/Dijkstra.java
+++ graph/Dijkstra.java
@@ -5,20 +5,22 @@ package algorithm.graph;
     그래프의 특정 노드에서 출발하여 그래프 내의 모든 다른 노드에 도착하는 최단 경로를 찾는 문제
 
     Dijkstra Algorithm : 하나의 정점에서 다른 모든 정점에 도착하는 최단 경로를 구하는 문제
-    - 첫 정점을 기준으로 연결되어있느 정점들을 추가하며 최단 거리를 갱신(update)
-    - BFS(breadth first search)와 유사
-        => 거리를 저장하는 배열 만든 후 인접 노드간의 거리부터 먼저 계산
-           첫 정점에서 해당하는 노드간의 가장 짧은 거리를 배열에 update
+        - 첫 정점을 기준으로 연결되어있느 정점들을 추가하며 최단 거리를 갱신(update)
+        - BFS(breadth first search)와 유사
+            => 거리를 저장하는 배열 만든 후 인접 노드간의 거리부터 먼저 계산
+               첫 정점에서 해당하는 노드간의 가장 짧은 거리를 배열에 update
 
     1. 첫 정점을 기준으로 각 정점까지의 거리를 저장하는 배열 선언
         - 첫 정점 0, 나머지는 INF(무한대)
         - 우선순위 큐(Min Heap)에 첫 정점을 넣음
+
     2. 우선순위 큐(Min Heap)에서 노드를 꺼냄
         - 처음에는 첫 정점만 연결되어 이것이 꺼내짐
         - 꺼낸 정점의 인접 노드에 대해 배열에 저장된 거리와 비교하여 최솟값으로 배열 갱신
         - 배열이 갱신된 경우 해당 인접 노드를 우선순위 큐에 삽입
             => 결과적으로 BFS 와 유사하게 인접 노드들을 순차적으로 방문하게 된다.
             => 만약 배열에 저장된 최솟값보다 더 큰 가중치를 가지는 경우 해당 노드와 인접 노드의 거리 계산 안함
+
     3. 2번 과정을 우선순위 큐가 빌 때 까지 반복
 
     Dijkstra 알고리즘 시간복잡도
@@ -58,7 +60,7 @@ public class Dijkstra {
 
     public static void main(String[] args) {
         Dijkstra d = new Dijkstra();
-        d.init_graph();
+        d.initGraph();
         System.out.println(d.dijkstraFunc("A"));
     }
 
@@ -102,7 +104,28 @@ public class Dijkstra {
         return distance;
     }
 
-    public void init_graph() {
+    static class Edge implements Comparable<Edge> {
+        public int distance;
+        public String vertex;
+
+        public Edge(int distance, String vertex) {
+            this.distance = distance;
+            this.vertex = vertex;
+        }
+
+        // System.out.println() 으로 객체 자체 출력 시
+        @Override
+        public String toString() {
+            return "vertex: " + this.vertex + ", distance: " + this.distance;
+        }
+
+        @Override
+        public int compareTo(Edge edge) {
+            return this.distance - edge.distance;
+        }
+    }
+
+    public void initGraph() {
         graph.put("A", new ArrayList<>(Arrays.asList(new Edge(8, "B"), new Edge(1, "C"), new Edge(2, "D"))));
         graph.put("B", new ArrayList<>());
         graph.put("C", new ArrayList<>(Arrays.asList(new Edge(5, "B"), new Edge(2, "D"))));
